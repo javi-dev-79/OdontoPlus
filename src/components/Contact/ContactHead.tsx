f13beb043a7c3433769ff62b21cb6761848f7b28
Javi <javi.dev.79@gmail.com>
2025-01-31 00:30:04 +0000

    feat: add title and text component for Contact page

---
+++ src/components/Contact/ContactHead.tsx
@@ -0,0 +1,19 @@
+import '../../styles/Contact.css' // Importar el archivo CSS
+
+const ContactHead = () => {
+  return (
+    <div className='contact-head-container'>
+      <h1 className='contact-head-title'>CONTÁCTANOS</h1>
+      <h2 className='contact-head-subtitle'>
+        RESERVA UNA CITA O SOLICITA INFORMACIÓN
+      </h2>
+      <p className='contact-head-description'>
+        Nuestro Centro de Atención te atenderá de lunes a jueves de 10h a 18h y
+        los viernes de 8h a 16h. Tu petición es muy importante para nosotros, te
+        responderemos lo antes posible.
+      </p>
+    </div>
+  )
+}
+
+export default ContactHead
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,13 @@
+import ContactHead from '../components/Contact/ContactHead'
+
 const Contact = () => {
-  return <h1>Contact Us</h1>
+  return (
+    <main>
+      <div className='full-width-section'>
+        <ContactHead />
+      </div>
+    </main>
+  )
 }
 
 export default Contact
