87fe0a7ad3ec394bd8d179ae1a2c3ef579d5cbd5
Javi <javi.dev.79@gmail.com>
2025-01-31 10:41:24 +0000

    feat: add contact form implementation

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,14 @@
+import ContactForm from '../components/Contact/ContactForm'
 import ContactHead from '../components/Contact/ContactHead'
+import ContactInfo from '../components/Contact/ContactInfo'
 
 const Contact = () => {
   return (
     <main>
       <div className='full-width-section'>
         <ContactHead />
+        <ContactForm />
+        <ContactInfo />
       </div>
     </main>
   )
