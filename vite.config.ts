67d11d1e09cc987c03932e7e22a01a00c7d9bd13
javi-dev-79 <javi.dev.79@gmail.com>
2025-03-08 21:02:53 +0000

    feat: Chakra-ui configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   // TODO: eliminar cuando termine el server
   server: {
     host: true, // Esto permite que el servidor escuche en todas las interfaces de red
